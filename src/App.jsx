8c474324007fbb72a9b558faed96f32f09d01e3c
Shema Elisa <shemaElyssa@gmail.com>
2024-08-02 20:08:11 +0000

    styling..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  
+    function buttonPress() {
+
+    }
+    return <div className='container-fluid'><div className='content'>
+        <h1>Expression</h1>
+        <div className='dispaly'>
+
+            <div className='expression'></div>
+            <div className='answer' id='answer'></div>
+        </div>
+        
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+    </div></div>
 
 }
 
