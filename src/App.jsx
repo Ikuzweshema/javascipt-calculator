7c9decd6c91e39b74a39b4de4ca9816655f29c54
Shema Elisa <shemaElyssa@gmail.com>
2024-08-02 20:54:28 +0000

    designing...

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,40 @@ function App() {
     function buttonPress() {
 
     }
-    return <div className='container-fluid'><div className='content'>
+    return <div className='container-fluid d-flex justify-content-center '><div className='contentaier '>
         <h1>Expression</h1>
-        <div className='dispaly'>
-
-            <div className='expression'></div>
-            <div className='answer' id='answer'></div>
+        <div className=' d-flex flex-column border p-2 gap-1' >
+            <div className=" d-flex gap-1">
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-danger px-3'>Clear</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-4'>%</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-primary px-4'>/</button>
+            </div>
+            <div className=' d-flex gap-1'>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>7</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >8</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>9</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-info px-4'>X</button>
+            </div>
+            <div className=' d-flex gap-1'>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>4</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >5</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>6</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-warning px-4'>-</button>
+            </div>
+            <div className=' d-flex gap-1'>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>1</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >2</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>3</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-info px-4'>+</button>
+            </div>
+            <div className=' d-flex gap-1'>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>7</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >8</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>9</button>
+                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-info px-4'>X</button>
+            </div>
         </div>
-        
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
-        <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary'>C</button>
+
     </div></div>
 
 }
