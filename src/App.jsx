a2b2a4a9c547ce08b0e0c49999a2ba1a52ccbabf
Shema Elisa <shemaElyssa@gmail.com>
2024-08-02 21:06:51 +0000

    designing ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,69 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
 function App() {
-    function buttonPress() {
+  const [expression, setExpression] = useState('');
 
-    }
-    return <div className='container-fluid d-flex justify-content-center '><div className='contentaier '>
-        <h1>Expression</h1>
-        <div className=' d-flex flex-column border p-2 gap-1' >
-            <div className=" d-flex gap-1">
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-danger px-3'>Clear</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-4'>%</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-primary px-4'>/</button>
+  function buttonPress(value) {
+   
+  }
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6 col-lg-4">
+          <h1 className="text-center mb-4">Calculator</h1>
+          <div className="border p-3 rounded">
+            <div className="form-group mb-3">
+              <sapn>{expression}</sapn>
             </div>
-            <div className=' d-flex gap-1'>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>7</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >8</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>9</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-info px-4'>X</button>
+            <div className="row g-2">
+              {[
+                ['C', '%', '/', 'X'],
+                ['7', '8', '9', '-'],
+                ['4', '5', '6', '+'],
+                ['1', '2', '3', '='],
+                ['0', '.', '']
+              ].map((row, rowIndex) => (
+                <div className="row g-2" key={rowIndex}>
+                  {row.map((buttonText, buttonIndex) => (
+                    <div className={`col-${buttonText === '0' ? '6' : '3'}`} key={buttonIndex}>
+                      <button
+                        type="button"
+                        className={`btn btn-${getButtonClass(buttonText)} w-100`}
+                        onClick={() => buttonPress(buttonText)}
+                        disabled={!buttonText}
+                      >
+                        {buttonText}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              ))}
             </div>
-            <div className=' d-flex gap-1'>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>4</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >5</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>6</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-warning px-4'>-</button>
-            </div>
-            <div className=' d-flex gap-1'>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>1</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >2</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>3</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-info px-4'>+</button>
-            </div>
-            <div className=' d-flex gap-1'>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>7</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3' >8</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-secondary px-3'>9</button>
-                <button type='button' id='clear' onClick={() => buttonPress()} className='btn btn-info px-4'>X</button>
-            </div>
+          </div>
         </div>
-
-    </div></div>
-
+      </div>
+    </div>
+  );
 }
 
-export default App
+function getButtonClass(buttonText) {
+  switch (buttonText) {
+    case 'C':
+      return 'danger';
+    case '%':
+    case '/':
+    case 'X':
+    case '+':
+    case '-':
+      return 'primary';
+    case '=':
+      return 'success';
+    default:
+      return 'secondary';
+  }
+}
+
+export default App;
