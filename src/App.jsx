de9d77e0705896b236bca917c1aaca7b10a6d450
Shema Elisa <shemaElyssa@gmail.com>
2024-08-03 06:32:51 +0000

    calculations..

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,68 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 
 function App() {
-    const [expression, setExpression] = useState(0);
-    const [answer, setAnswer] = useState(0)
+    const [expression, setExpression] = useState("");
+    const [answer, setAnswer] = useState("")
+    const trimed = expression.trim()
+    function isOperator(symbol) {
+        return /[+\-*/%]/.test(symbol);
+    }
     function buttonPress(key) {
-         if (key=="C") {
+        if (key == "C") {
             setAnswer(0)
             setExpression("")
-         }
-         else if(key=="-"){
-            if(answer=="") return;
+        }
+        else if (key == "-") {
+            if (answer == "") return;
             setAnswer(
-                answer.toString().charAt(0)==="-"? answer.slice(1):"-"+answer
+                answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
             )
-         }
-    }
+        }
+        else if (key == "%") {
+            if (answer === "") return;
+            setAnswer((parseFloat(answer) / 100).toString());
 
+        }
+        else if (isOperator(key)) {
+            setExpression(trimed + " " + key + " ")
+        }
+        else if (key == "=") {
+            calculate();
+        }
+        else if (key === "0") {
+            if (expression.charAt(0) !== "0") {
+                setExpression(expression + key)
+            }
+        }
+        else if (key == ".") {
+           const lastNumber=expression.split(/[+\-*/%]/g).pop()
+            setExpression(answer + key)
+
+        }
+    }
+    function calculate() {
+
+    }
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
                 <div className="col-12 col-md-6 col-lg-4">
-                    <h1 className="text-center mb-4">Calculator</h1>
+
                     <div className="border p-3 rounded">
                         <div className="d-flex  justify-content-end mb-3">
 
                             <div className="row">
                                 <span>{expression}</span>
-                                <span style={{fontSize:40}}>{answer}</span>
-                            </div>  
+                                <span style={{ fontSize: 40 }}>{answer}</span>
+                            </div>
                         </div>
                         <div className="row g-2">
                             {[
-                                ['C', '%', '/', 'X'],
+                                ['C', '%', , '-/+', 'X'],
                                 ['7', '8', '9', '-'],
                                 ['4', '5', '6', '+'],
-                                ['1', '2', '3', '='],
-                                ['0', '.', ]
+                                ['1', '2', '3', , '/',],
+                                ['0', '.', , '=']
                             ].map((row, rowIndex) => (
                                 <div className="row g-2" key={rowIndex}>
                                     {row.map((buttonText, buttonIndex) => (
